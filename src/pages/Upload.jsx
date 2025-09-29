db2754b3c3a4af16f1b9c461e00119303311a382
Aditya Gaonkar <142329059+AdiGaonkar@users.noreply.github.com>
2025-09-29 15:25:58 +0530

    Update Upload.jsx

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import api from "../api";
+import api from "../api.js";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
